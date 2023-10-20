080757d1c363917436ba6bedd1f468ee0d34e129
Garry <vorzel47051@gmail.com>
2023-10-20 21:22:03 +0300

    js-hw-06

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ulNew = document.querySelector("#ingredients");
+ingredients.map((ingredient) => {
+  const listNew = document.createElement("li");
+  console.log(listNew);
+  listNew.textContent = ingredient;
+  listNew.classList.add("item");
+  ulNew.appendChild(listNew);
+});
