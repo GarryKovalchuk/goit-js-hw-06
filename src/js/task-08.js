3fa7b8ecfc2236738ed7789d6cdfffd595c09a0b
Garry <vorzel47051@gmail.com>
2023-10-23 21:44:36 +0300

    js-hw-06

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (
+    emailInput.value === "" ||
+    passwordInput.value === ""
+  ) {
+    alert("All friends must be filled out");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+    console.log(formData);
+    loginForm.requestFullscreen();
+  }
+});
