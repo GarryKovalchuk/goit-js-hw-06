7c86dc588fdf54026fed966bca90694e303ab823
Garry <vorzel47051@gmail.com>
2023-10-25 20:48:52 +0300

    js-hw-06

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,12 @@ const ingredients = [
   "Condiments",
 ];
 const ulNew = document.querySelector("#ingredients");
-ingredients.map((ingredient) => {
+const items = ingredients.map((ingredient) => {
   const listNew = document.createElement("li");
   console.log(listNew);
   listNew.textContent = ingredient;
   listNew.classList.add("item");
-  ulNew.appendChild(listNew);
+  /*ulNew.appendChild(listNew);*/
+  return listNew;
 });
+ulNew.append(...items);
